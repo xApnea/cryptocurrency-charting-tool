a5df12b7f0d5a66fb50074bbe4b017d626532741
xApnea <jcbruno150@gmail.com>
2020-07-29 18:48:09 -0400

    React component rendering canvas for chartjs

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,18 @@
+import React from 'react';
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
 
+    }
+  }
+
+  render () {
+    return (
+      <canvas id="myChart" width="400" height="400"></canvas>
+    )
+  }
+}
+
+export default App;
