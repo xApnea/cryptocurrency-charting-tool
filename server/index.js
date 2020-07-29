4bad5418bc7d950814224422f1a27830c412ad96
xApnea <jcbruno150@gmail.com>
2020-07-29 14:29:41 -0400

    Uses start and end date queries in request to coin api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 
@@ -10,7 +11,23 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 
 app.get('/api', (req, res) => {
-  res.status(200).send('Request was good');
+  const start = req.query.start;
+  const end = req.query.end;
+
+  console.log(start);
+  console.log(end);
+
+  const path = (start && end) ? `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}` : 'https://api.coindesk.com/v1/bpi/historical/close.json';
+
+  axios.get(path)
+    .then((response) => {
+      console.log(response.data);
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send(error);
+    });
 })
 
 // https://api.coindesk.com/v1/bpi/historical/close.json
