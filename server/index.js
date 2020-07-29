1fcf7c34c245c2ad89517ac744a7f7091bab2270
xApnea <jcbruno150@gmail.com>
2020-07-29 13:55:59 -0400

    Server initialized

--- server/index.js
+++ server/index.js
@@ -1,2 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/api', (req, res) => {
+  res.status(200).send('Request was good');
+})
+
+// https://api.coindesk.com/v1/bpi/historical/close.json
+// https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
 
 
+const port = process.env.PORT || 3000;
+var server = app.listen(port, () => {console.log(`Listening on port: ${port}`)});
+
+module.exports = server;
